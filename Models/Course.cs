38c0b2254e3021ead0587c0f75d37ba8141a2655
Loon-7w7 <99504287+Loon-7w7@users.noreply.github.com>
2022-05-17 17:00:29 -0500

    Cambios antes del reto

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,17 @@ namespace EscuelaPlatazi.Models
 {
     public class Course : ObjectSchoolBase
     {
-
+        [Required]
+        public override string Name { get; set; }
+        [Required]
+        [Display(Name = "Day Type", ShortName = "Type")]
+        [EnumDataType(typeof(TypesDay))]
         public TypesDay TypeOfDay { get; set; }
 
         public List<SubjectC> ListOfSubjects { get; set; }
         public List<Student> StudentList { get; set; }
         public string Direcion { get; set; }
+        [Display(Name = "School", Prompt = "Provide a Name")]
         public string SchoolId { get; set; }
         public School school { get; set; }
 
